3a64879a120a5cc06e87cd7cc0bcfe33db94d131
Niels Mündler <n.muendler@tum.de>
2021-06-20 16:43:33 +0200

    Fix build instructions

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ USER_AGENT="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Fire
 
 
 nativefier \
-	--inject "${pkgname}.inject.js" \
+	--inject "${pkgname}-inject.js" \
 	--single-instance \
-	--user-agent ${USER_AGENT} \
+	--user-agent "${USER_AGENT}" \
 	--icon "${pkgname}.png" \
 	--browserwindow-options '{ "webPreferences": { "spellcheck": true } }' \
 	"https://web.whatsapp.com"
