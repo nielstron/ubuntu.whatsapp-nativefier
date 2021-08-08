775744fd46ae6049a997a7f8a5725ea8b367ee05
Niels Mündler <n.muendler@tum.de>
2021-08-08 02:40:08 +0200

    Fix injection whatsapp

--- whatsapp-nativefier-inject.js
+++ whatsapp-nativefier-inject.js
@@ -1,7 +1,22 @@
-if ('serviceWorker' in navigator) {
-    caches.keys().then(function (cacheNames) {
-        cacheNames.forEach(function (cacheName) {
-            caches.delete(cacheName);
-        });
-    });
-}
+
+// unregister serviceworker after startup -> don't interfere at next startup
+let deleteFun = function(){
+  if("serviceWorker" in navigator){
+    navigator.serviceWorker.getRegistrations().then(function(e){
+     //console.log(e);
+     for(r of e){
+       r.unregister();
+     }
+   });
+    //try{
+    //  navigator.serviceWorker.ready.then(function(reg){
+    //      reg.unregister();
+    //  }).catch(function(error){
+    //      console.log(error);
+    //   });
+    //} catch(e) {
+    //    console.log(e);
+    //}
+  }};
+deleteFun();
+setInterval(deleteFun,1000);
