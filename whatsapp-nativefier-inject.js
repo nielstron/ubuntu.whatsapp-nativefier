4fb2cd632cd48d9065dc74a9d7e3f75d2383825c
Niels Mündler <n.muendler@tum.de>
2022-02-11 11:39:32 +0100

    Use archlinux's version for injection
    commmit f399849b04cc480709c0b772a5925f791fc724a2

--- whatsapp-nativefier-inject.js
+++ whatsapp-nativefier-inject.js
@@ -1,14 +1,46 @@
+const TIME_BETWEEN_INVOCATIONS = 5000; // Time between invocations (ms)
+const NUMBER_OF_INVOCATIONS = 5;       // Number of invocations
 
-function recheck () {
-    navigator.serviceWorker.getRegistrations().then((registrations) => {
-        for(registration of registrations){
-             registration.unregister();
+console.info("Checking time between invocations:", TIME_BETWEEN_INVOCATIONS, ", Number of invocations:", NUMBER_OF_INVOCATIONS);
+
+let invocationNumber = 0;
+for(var number = 0; number < NUMBER_OF_INVOCATIONS; number++) {
+    setTimeout(function() {
+        console.info("*****", ++invocationNumber, "******")
+        console.info("Checking for service worker in navigator...");
+        if ("serviceWorker" in navigator) {
+            console.info("Service worker found in navigator. Checking for unsupported browser message...");
+            if (document.querySelector("a[href='https://support.google.com/chrome/answer/95414']")) {
+                console.info("Checking cache names from service worker...");
+                caches.keys().then(function (cacheNames) {
+                    console.info("CacheNames:", cacheNames);
+                    cacheNames.forEach(function (cacheName) {
+                        console.info("Deleting cache", cacheName, "...");
+                        caches.delete(cacheName).then(function (result) {
+                            console.info("Cache", cacheName, "deleted:", result);
+                        });
+                    });
+                });
+
+                console.info("Checking service worker registrations...");
+                navigator.serviceWorker.getRegistrations().then(function(registrations) {
+                    console.info("Registrations:", registrations);
+                    registrations.forEach(function (registration) {
+                        console.info("Unregistering registration", registration, "...");
+                        registration.unregister().then(function(result) {
+                            console.info("Registration", registration, "unregistered:", result);
+                        });
+                    });
+                });
+
+                setTimeout(function() {
+                    document.location.reload();
+                });
+            } else {
+                console.info("Unsupported browser message not found.");
+            }
+        } else {
+            console.info("Service worker not found in navigator.");
         }
-        if (document.querySelector("body[class='page-version']")) {
-            console.log("reload");
-            document.location.reload();
-         }
-    });
-    setTimeout(recheck, 5000); // callback
+    }, number * TIME_BETWEEN_INVOCATIONS);
 }
-recheck();
